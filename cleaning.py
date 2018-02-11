bdda551488ac6a22a7670e839705b28b2bc7549f
roguexray007 <hardxray007@gmail.com>
2018-02-11 14:08:26 +0530

    cleaning a directory(eg desktop,downloads)
    sort the files on Desktop (u can actually specify the directory using -s option) on the basis of file extension and move them in
    separate folders in Documents folder(can specify thee directory where u want files to be reorganised using -d option).

---
+++ cleaning.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+import sys
+import optparse
+import subprocess
+import os
+import shutil
+
+# ls = subprocess.Popen(["ls", "-p", "."],  
+#                       stdout=subprocess.PIPE,
+#                      )
+
+# # define the grep command
+# grep = subprocess.Popen(["grep", "-v", "/$"],  
+#                         stdin=ls.stdout,
+#                         stdout=subprocess.PIPE,
+#                         )
+
+# # read from the end of the pipe (stdout)
+# endOfPipe = grep.stdout
+
+# # output the files line by line
+# for line in endOfPipe:  
+#     print (line.decode(),end=' ')
+
+ 
+ #---------------------------------------------------------------------   
+# new_dir='/Users/hardxray007/Desktop/new_dir' --sample test run path
+
+parser=optparse.OptionParser()
+parser.add_option('-s','--src',dest='path',help='path of the source directory to reorganise')
+parser.add_option('-d','--dest',dest='new_dir',help='path of the destination directory where u want files to be reorganised')
+
+(options,args)=parser.parse_args()
+
+if options.path is None:
+    options.path = input('Enter path of the source directory to reorganise:')
+
+if options.new_dir is None:
+    options.new_dir = input('Enter path of the destination directory where u want files to be reorganised')
+
+
+if not os.path.exists(options.new_dir):
+	os.makedirs(options.new_dir)
+
+#path='/Users/hardxray007/Desktop' sample directory to reorganize
+for root, dirs, files in os.walk(options.path):  
+	if root!=options.path:
+		continue
+	files[:]=[f for f in files if not f.startswith('.')]
+	for filename in files:
+		print(filename)
+		if "." not in filename:
+			ext='.without_ext'
+		else:
+			ind=filename.rfind('.')+1
+			ext=filename[ind:]
+		copy_dir_path=options.new_dir+'/'+ext.upper()
+		if not os.path.exists(copy_dir_path):
+			os.makedirs(copy_dir_path)
+		shutil.copy(root+'/'+filename,copy_dir_path+'/'+filename)
+		#if u want to move use shutil.move instead of shutil.copy
+		# shutil.move(root+'/'+filename,copy_dir_path+'/'+filename)
+
+
