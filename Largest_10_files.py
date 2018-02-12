f49fec4f531ce9e063e075f3640e65ee6a457095
Kunal Sikri <hardxray007@gmail.com>
2018-02-12 16:02:10 +0530

    Largest.py updated
    compatability for linux and mac

--- Largest_10_files.py
+++ Largest_10_files.py
@@ -9,10 +9,10 @@ parser.add_option('-p','--path',dest='path',help='path of the directory')
 
 if options.path is None:
     options.path = input('Enter path of directory:')
-
-# cmd1='find %s -exec du -a -h {} \;|sort -rh|grep ".*\.[a-zA-Z]"|uniq|head -n 10' % options.path  
-#more optimized version
-cmd1='find %s -not -type d -exec ls -sh  {} \;|sort -rh|head -n 10' % options.path
+#in case of mac use cmd2 (in mac ls -sh not displaying size in human readable format
+# cmd2='find %s -exec du -a -h {} \;|sort -rh|grep ".*\.[a-zA-Z]"|uniq|head -n 10' % options.path  
+#more optimized version for linux
+cmd1='find %s -not -type d -exec ls -sh  {} \;|sort -rh|head -n 10' % options.path #for linux
 proc=subprocess.call(cmd1,shell=True)
 
 
